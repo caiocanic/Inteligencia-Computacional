1d9e0c2857e9fc702f89f139af0e0328cdfc67c6
caiocanic <caiocanic@outlook.com>
2017-09-20 18:22:29 -0300

    Implemented bissection method to find optimal alpha

--- GradientDescent/calc_alfa.m
+++ GradientDescent/calc_alfa.m
@@ -1,34 +1,30 @@
-%Dúvida: O que é a e b? Está certo do jeito implementado?
-%Pedir código do professor.
+function alfa=calc_alfa(X,d,type)
+	if (type == "golden")
+		alfa=golden_section(X,d);
+	else
+		alfa=bisection(X,d);
+	end
+	fprintf('alfa = %2.7f\n', alfa);
+end
 
-function alfa=calc_alfa(X,d)
+function alfa=golden_section(X,d)
 	r=(-1+sqrt(5))/2;
 	a = 0;
 	b = rand;
-	
+
 	%Checa se b torna hLinha positivo. Caso não, drobra o valor de b e
 	%repete a checagem.
-	Xn = X + b*d;
-	[~,g,~] = calc_func(Xn);
-	hLinha = g'*d;
+	hLinha = calc_hLinha(b,X,d);
 	while hLinha <=0
 		b = 2*b;
-		Xn = X + b*d;
-		[~,g,~] = calc_func(Xn);
-		hLinha = g'*d;
+		hLinha = calc_hLinha(b,X,d);
 	end
-	
 	alfa1 = b - r*(b-a);
 	alfa2 = a + r*(b-a);
 	
-	while (alfa2-alfa1)/2 > 1.0e-6
-		X1 = X + alfa1*d;
-		[~,g,~] = calc_func(X1);
-		hLinhaX1 = g'*d;
-		X2 = X + alfa2*d;
-		[~,g,~] = calc_func(X2);
-		hLinhaX2 = g'*d;
-		%fprintf("a: %2.5f b: %2.5f  alfa1: %2.5f alfa2: %2.5f hLX1: %2.5f hLX2: %2.5f\n",a,b,alfa1,alfa2,hLinhaX1,hLinhaX2);
+	while (alfa2-alfa1)/2 > 1.0e-4
+		hLinhaX1 = calc_hLinha(alfa1,X,d);
+		hLinhaX2 = calc_hLinha(alfa2,X,d);
 		if hLinhaX1*hLinhaX2 < 0
 			b = alfa2;
 			alfa2 = alfa1;
@@ -45,7 +41,39 @@ function alfa=calc_alfa(X,d)
 			end
 		end
 	end
-	
 	alfa = (alfa1 + alfa2)/2;
-	fprintf('alfa = %2.7f\n', alfa);
+end
+
+function alfa=bisection(X,d)
+	a = 0;
+	b = rand;
+	
+	%Checa se b torna hLinha positivo. Caso não, drobra o valor de b e
+	%repete a checagem.
+	hLinha = calc_hLinha(b,X,d);
+	while hLinha <= 0
+		b = 2*b;
+		hLinha = calc_hLinha(b,X,d);
+	end
+	alfaM=(a+b)/2;
+	hLinhaM = calc_hLinha(alfaM,X,d);
+	
+	while abs(hLinhaM) > 1.0e-4
+		%fprintf("a: %2.5f b: %2.5f alfaM: %2.5f\n",a,b,alfaM);
+		pause
+		if hLinhaM > 0
+			b=alfaM;
+		else
+			a=alfaM;
+		end
+		alfaM=(a+b)/2;
+		hLinhaM = calc_hLinha(alfaM,X,d);
+	end
+	alfa=alfaM;
+end
+
+function hLinha=calc_hLinha(alpaTest,X,d)
+	Xn = X + alpaTest*d;
+	[~,g,~] = calc_func(Xn);
+	hLinha = g'*d;
 end

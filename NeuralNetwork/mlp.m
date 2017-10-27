30d823bbd82313dde1b923bdfc893e0a56eb12e7
caiocanic <caiocanic@outlook.com>
2017-10-27 02:09:34 -0200

    The MSE values from each iteration are now stored to be ploted in the end of training
    Closes #1

--- NeuralNetwork/mlp.m
+++ NeuralNetwork/mlp.m
@@ -1,36 +1,37 @@
 function [A,B,Y] = mlp(Xtr,Yd,Xvl,Xts,h)
-	
 	[A,B] = treinamento(Xtr,Yd,Xvl,h);
 	Y = teste(A,B,Xts);
 end
 
 function [A,B] = treinamento(Xtr,Yd,Xvl,h)
-	nepMax = 20000;
-	nep = 0;
 	alfa = 0.1;
-	
+	nepMax = 20000;
+	nep = 1;
 	[N,ne] = size(Xtr);
 	ns = size(Yd,2);
 	%Adiciona o bias
 	Xtr = [Xtr, ones(N,1)];
-	
 	%Inicializa as matrizes de pesos A e B
 	A = rands(h,ne+1)/5;
 	B = rands(ns,h+1)/5;
 	
 	Y = calc_saida(A,B,Xtr,N);
 	erro = Y-Yd;
-	EQM = 1/N*sum(sum(erro.*erro));
-	while EQM > 1.0e-5 && nep < nepMax
+	EQM(nep) = 1/N*sum(sum(erro.*erro));
+	while EQM(nep) > 1.0e-5 && nep < nepMax
 		nep = nep+1;
 		[dJdA, dJdB] = calc_grad(Xtr,Yd,A,B,N);
+		%alfa = calc_alfa(A,B,dJdA,dJdB,Xtr,Yd,N);
 		A = A - alfa*dJdA;
 		B = B - alfa*dJdB;
 		Y = calc_saida(A,B,Xtr,N);
 		erro = Y-Yd;
-		EQM = 1/N*sum(sum(erro.*erro));
-		fprintf("EQM: %f\n",EQM);
+		EQM(nep) = 1/N*sum(sum(erro.*erro));
+		fprintf("EQM: %f\n",EQM(nep));
+		
+		%Validação
 	end
+	plot(EQM);
 end
 
 function Y = teste(A,B,Xts)

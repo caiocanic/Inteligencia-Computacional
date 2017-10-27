a0759867a6224f8eedb03a8435ac653593c71f61
caiocanic <caiocanic@outlook.com>
2017-10-27 00:14:45 -0200

    Moved calc_grad function to a different file

---
+++ NeuralNetwork/calc_grad.m
@@ -0,0 +1,12 @@
+function [dJdA, dJdB] = calc_grad(X,Yd,A,B,N)
+	Zin = X*A';
+	Z = tanh(Zin);
+	Yin = [Z,ones(N,1)]*B';
+	Y = tanh(Yin);
+	erro = Y-Yd;
+	
+	dJdB = 1/N*(erro.*(1-Y.*Y))'*[Z,ones(N,1)];
+	dJdZ = (erro.*(1-Y.*Y))*B;
+	dJdZ = dJdZ(:,1:end-1);
+	dJdA = 1/N*(dJdZ.*(1-Z.*Z))'*X;
+end
--- NeuralNetwork/mlp.m
+++ NeuralNetwork/mlp.m
@@ -48,16 +48,3 @@ function Y = calc_saida(A,B,X,N)
 	Yin = [Z,ones(N,1)]*B';
 	Y = tanh(Yin);
 end
-
-function [dJdA, dJdB] = calc_grad(X,Yd,A,B,N)
-	Zin = X*A';
-	Z = tanh(Zin);
-	Yin = [Z,ones(N,1)]*B';
-	Y = tanh(Yin);
-	erro = Y-Yd;
-	
-	dJdB = 1/N*(erro.*(1-Y.*Y))'*[Z,ones(N,1)];
-	dJdZ = (erro.*(1-Y.*Y))*B;
-	dJdZ = dJdZ(:,1:end-1);
-	dJdA = 1/N*(dJdZ.*(1-Z.*Z))'*X;
-end

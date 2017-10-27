9be9ce275f7db50464b5da005fe8d5deb476c195
caiocanic <caiocanic@outlook.com>
2017-10-27 00:10:02 -0200

    Test implemented. Closes #2

--- NeuralNetwork/mlp.m
+++ NeuralNetwork/mlp.m
@@ -1,31 +1,43 @@
-function [A,B,Y] = mlp(X,d,h)
-	[N,ne] = size(X);
-	ns = size(d,2);
+function [A,B,Y] = mlp(Xtr,Yd,Xvl,Xts,h)
+	
+	[A,B] = treinamento(Xtr,Yd,Xvl,h);
+	Y = teste(A,B,Xts);
+end
 
+function [A,B] = treinamento(Xtr,Yd,Xvl,h)
+	nepMax = 20000;
+	nep = 0;
+	alfa = 0.1;
+	
+	[N,ne] = size(Xtr);
+	ns = size(Yd,2);
 	%Adiciona o bias
-	X = [X, ones(N,1)];
-
+	Xtr = [Xtr, ones(N,1)];
+	
 	%Inicializa as matrizes de pesos A e B
 	A = rands(h,ne+1)/5;
 	B = rands(ns,h+1)/5;
 	
-	Y = calc_saida(A,B,X,N);
-	erro = Y-d;
+	Y = calc_saida(A,B,Xtr,N);
+	erro = Y-Yd;
 	EQM = 1/N*sum(sum(erro.*erro));
-	nepMax = 30000;
-	nep = 0;
-	alfa = 0.3;
-
 	while EQM > 1.0e-5 && nep < nepMax
 		nep = nep+1;
-		[dJdA, dJdB] = calc_grad(X,d,A,B,N);
+		[dJdA, dJdB] = calc_grad(Xtr,Yd,A,B,N);
 		A = A - alfa*dJdA;
 		B = B - alfa*dJdB;
-		Y = calc_saida(A,B,X,N);
-		erro = Y-d;
+		Y = calc_saida(A,B,Xtr,N);
+		erro = Y-Yd;
 		EQM = 1/N*sum(sum(erro.*erro));
 		fprintf("EQM: %f\n",EQM);
 	end
+end
+
+function Y = teste(A,B,Xts)
+	[N,~] = size(Xts);
+	%Adiciona o bias
+	Xts = [Xts, ones(N,1)];
+	Y = calc_saida(A,B,Xts,N);
 end
 
 function Y = calc_saida(A,B,X,N)
@@ -37,12 +49,12 @@ function Y = calc_saida(A,B,X,N)
 	Y = tanh(Yin);
 end
 
-function [dJdA, dJdB] = calc_grad(X,d,A,B,N)
+function [dJdA, dJdB] = calc_grad(X,Yd,A,B,N)
 	Zin = X*A';
 	Z = tanh(Zin);
 	Yin = [Z,ones(N,1)]*B';
 	Y = tanh(Yin);
-	erro = Y-d;
+	erro = Y-Yd;
 	
 	dJdB = 1/N*(erro.*(1-Y.*Y))'*[Z,ones(N,1)];
 	dJdZ = (erro.*(1-Y.*Y))*B;

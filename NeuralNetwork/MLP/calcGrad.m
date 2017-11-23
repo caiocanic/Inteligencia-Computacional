9df96a44122a9e9c4a6a66aaca85a29d523b2bb8
caiocanic <caiocanic@outlook.com>
2017-11-23 01:58:49 -0200

    Adicionado comentários

--- NeuralNetwork/MLP/calcGrad.m
+++ NeuralNetwork/MLP/calcGrad.m
@@ -1,3 +1,15 @@
+%{
+Função responsável pelo cálculo dos gradientes das matrizes de pesos A e B.
+Parâmetros
+X: Conjunto de dados de entrada;
+Yd: Saída desejada para o conjunto de dados X;
+A: Matriz de pesos da entrada;
+B: Matriz de pesos da camada de saída;
+N: Número de instâncias do conjunto de entrada;
+Saídas
+dJdA: Gradiente da matriz de pesos A;
+dJdB: Gradiente da matriz de pesos B;
+%}
 function [dJdA, dJdB] = calcGrad(X,Yd,A,B,N)
 	Zin = X*A';
 	Z = tanh(Zin);

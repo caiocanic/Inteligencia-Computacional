3ea17f92e0c1ab0006c5cac483eadf885dd9d3b6
caiocanic <caiocanic@outlook.com>
2017-12-06 00:56:35 -0200

    Adicionado uma função para testar a RNN com parâmetros específicos.

--- RedesNeurais/RedeRecorrente/teste.m
+++ RedesNeurais/RedeRecorrente/teste.m
@@ -4,23 +4,17 @@ um treinamento e teste para os dados parâmetros
 %}
 function teste()
 	hMax=6;
-	nepMax=10000;
+	nepMax=5000;
 	alfaInicial = 0.1;
 	lagMax = 7;
 	nroTestes = 10;
 	porcValidacao = 0.3;
-	%testaRede(hMax, nepMax, alfaInicial, lagMax, nroTestes, porcValidacao);
+	%testaParametros(hMax, nepMax, alfaInicial, lagMax, nroTestes, porcValidacao);
 	
 	%
-	h=2;
-	lag=3;
-	datasetTreinamento = load("Dataset_series/serie1_trein.txt");
-	datasetTeste = load("Dataset_series/serie1_test.txt");
-	[Xtr,Ydtr,Xvl,Ydvl,Xts,Ydts] = processaDados(datasetTreinamento, datasetTeste,porcValidacao);
-	rede = RedeRecorrente(h,lag,nepMax,alfaInicial(1));
-	treinamento(rede,Xtr,Ydtr,Xvl,Ydvl)
-	EQMts = teste(rede,Xts,Ydts);
-	disp(EQMts);
+	h=6;
+	lag=7;
+	testaRede(3,h,lag,nepMax,alfaInicial,nroTestes,porcValidacao);
 	%}
 	%{
 	plot(datasetTeste(2:end,1),'DisplayName','dataset');
@@ -50,17 +44,17 @@ Saídas salvas em arquivos
 EQMmedioSerie.mat: Média dos EQM's testes realizados para uma dada série
 EQMdesvioSerie.mat: Desvio padrão dos EQM's dos testes realizados
 %}
-function testaRede(hMax, nepMax, alfaInicial, lagMax, nroTestes, porcValidacao)
+function testaParametros(hMax, nepMax, alfaInicial, lagMax, nroTestes, porcValidacao)
 	for serie=1:4
 		EQMmedio = zeros(hMax-1,lagMax+1);
 		EQMdesvio = zeros(hMax-1,lagMax+1);
-		EQMtemp = zeros(1,10);
+		EQMtemp = zeros(1,nroTestes);
 		datasetTreinamento = load("Dataset_series/serie" + serie + "_trein.txt");
 		datasetTeste = load("Dataset_series/serie"+ serie + "_test.txt");
 		for h=2:hMax
 			for lag=1:lagMax
 				fprintf("serie: %d h: %d lag: %d\n",serie, h,lag);
-				for i=1:nroTestes
+				parfor i=1:nroTestes
 					[Xtr,Ydtr,Xvl,Ydvl,Xts,Ydts] = processaDados(datasetTreinamento, datasetTeste,porcValidacao);
 					rede = RedeRecorrente(h,lag,nepMax,alfaInicial);
 					treinamento(rede,Xtr,Ydtr,Xvl,Ydvl);
@@ -68,9 +62,53 @@ function testaRede(hMax, nepMax, alfaInicial, lagMax, nroTestes, porcValidacao)
 				end
 				EQMmedio(h-1,lag+1) = mean(EQMtemp);
 				EQMdesvio(h-1,lag+1) = std(EQMtemp);
+				save("EQMmedioAlfaBissecaoSerie" + serie + ".mat","EQMmedio");
+				save("EQMdesvioAlfaBissecaoSerie" + serie + ".mat","EQMdesvio");
 			end
 		end
-		save("EQMmedioAlfaBissecaoSerie" + serie + ".mat","EQMmedio");
-		save("EQMdesvioAlfaBissecaoSerie" + serie + ".mat","EQMdesvio");
 	end
+end
+
+%{
+Função responsável por testar a RNN com valores definidos para h (número de
+neurónios) e lag (número de entradas atrasadas) para uma determinada série
+temporal. São executados <nroTestes> testes, sendo salvo em um arquivo a
+média dos EQM's e o desvio padrão desses testes.
+Parâmetros
+serie: Indice da série temporal para qual a rede será testada;
+h: Número de neurónios com o qual a rede será testada;
+lag: Número de entradas atrasadas para o qual a rede será testada;
+nepMax: Número máximo de épocas para o treinamento da rede que será
+testada;
+alfaInicial: valor do alfa inicial com o qual a rede será inicializada;
+nroTestes: Número de testes que serão realizados com a rede;
+porcValidacao: Porcentagens de dados de treinamento que serão utilizados
+para validação;
+Saídas salvas em arquivos
+EQMmedioSerie.mat: Média dos EQM's testes realizados para uma dada série
+EQMdesvioSerie.mat: Desvio padrão dos EQM's dos testes realizados
+nepConvergenciaMedia: Média de épocas necessárias para a rede convergir;
+nepConvergenciaDesvio: Desvio padrão do número de épocas para convergir;
+%}
+function testaRede(serie, h,lag,nepMax,alfaInicial,nroTestes,porcValidacao)
+	datasetTreinamento = load("Dataset_series/serie" + serie + "_trein.txt");
+	datasetTeste = load("Dataset_series/serie"+ serie + "_test.txt");
+	[Xtr,Ydtr,Xvl,Ydvl,Xts,Ydts] = processaDados(datasetTreinamento, datasetTeste,porcValidacao);
+	EQMtemp = zeros(1,nroTestes);
+	nepConvergenciaTemp = zeros(1,nroTestes);
+	for i=1:nroTestes
+		fprintf("serie: %d h: %d lag: %d teste: %d\n",serie, h,lag,i);
+		rede = RedeRecorrente(h,lag,nepMax,alfaInicial);
+		treinamento(rede,Xtr,Ydtr,Xvl,Ydvl)
+		EQMtemp(i) = teste(rede,Xts,Ydts);
+		nepConvergenciaTemp(i) = rede.nepConvergencia;
+	end
+	EQMmedio = mean(EQMtemp);
+	EQMdesvio = std(EQMtemp);
+	nepConvergenciaMedia = mean(nepConvergenciaTemp);
+	nepConvergenciaDesvio = std(nepConvergenciaTemp);
+	save("EQMmedio" + serie + ".mat","EQMmedio");
+	save("EQMdesvio" + serie + ".mat","EQMdesvio");
+	save("nepConvergenciaMedia" + serie + ".mat","nepConvergenciaMedia");
+	save("nepConvergenciaDesvio" + serie + ".mat","nepConvergenciaDesvio");
 end

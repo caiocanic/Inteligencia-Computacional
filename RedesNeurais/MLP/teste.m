2cf21170f44da2578ceb80c73c6695e86b288302
caiocanic <caiocanic@outlook.com>
2017-12-06 00:51:37 -0200

    Adicionar uma função para testar a mlp com parâmetros específicos.

--- RedesNeurais/MLP/teste.m
+++ RedesNeurais/MLP/teste.m
@@ -4,25 +4,17 @@ um treinamento e teste para os dados parâmetros
 %}
 function teste()
 	hMax=12;
-	nepMax=20000;
+	nepMax=5000;
 	alfaInicial=0.1;
 	lagMax = 15;
 	nroTestes = 10;
 	porcValidacao = 0.3;
-	%testaMlp(hMax, nepMax, alfaInicial, lagMax, nroTestes, porcValidacao);
+	testaParametros(hMax, nepMax, alfaInicial, lagMax, nroTestes, porcValidacao);
 	
 	%
-	h=5;
-	lag=6;
-	datasetTreinamento = load("Dataset_series/serie1_trein.txt");
-	datasetTeste = load("Dataset_series/serie1_test.txt");
-	[Xtr,Ydtr,Xvl,Ydvl,Xts] = processaDados(datasetTreinamento, datasetTeste, lag, porcValidacao);
-	mlp = Mlp(h,nepMax,alfaInicial);
-	mlp.treinamento(Xtr,Ydtr,Xvl,Ydvl);
-	mlp.teste(Xts);
-	erro = mlp.Y(1:end-1,1) - datasetTeste(2:end,1);
-	EQM = 1/size(Xts,1)*sum(sum(erro.*erro));
-	disp(EQM);
+	h=12;
+	lag=15;
+	%testaMlp(3,h,lag,nepMax,alfaInicial,nroTestes,porcValidacao)
 	%}
 	%{
 	plot(datasetTeste(2:end,1),'DisplayName','dataset');
@@ -52,7 +44,7 @@ Saídas salvas em arquivos
 EQMmedioSerie.mat: Média dos EQM's testes realizados para uma dada série
 EQMdesvioSerie.mat: Desvio padrão dos EQM's dos testes realizados
 %}
-function testaMlp(hMax, nepMax, alfaInicial, lagMax, nroTestes, porcValidacao)
+function testaParametros(hMax, nepMax, alfaInicial, lagMax, nroTestes, porcValidacao)
 	for serie=1:4
 		EQMmedio = zeros(hMax-1,lagMax+1);
 		EQMdesvio = zeros(hMax-1,lagMax+1);
@@ -77,4 +69,51 @@ function testaMlp(hMax, nepMax, alfaInicial, lagMax, nroTestes, porcValidacao)
 		save("EQMmedioAlfaBissecaoSerie" + serie + ".mat","EQMmedio");
 		save("EQMdesvioAlfaBissecaoSerie" + serie + ".mat","EQMdesvio");
 	end
+end
+
+%{
+Função responsável por testar a MLP com valores definidos para h (número de
+neurónios) e lag (número de entradas atrasadas) para uma determinada série
+temporal. São executados <nroTestes> testes, sendo salvo em um arquivo a
+média dos EQM's e o desvio padrão desses testes.
+Parâmetros
+serie: Indice da série temporal para qual a rede será testada;
+h: Número de neurónios com o qual a rede será testada;
+lag: Número de entradas atrasadas para o qual a rede será testada;
+nepMax: Número máximo de épocas para o treinamento da rede que será
+testada;
+alfaInicial: valor do alfa inicial com o qual a rede será inicializada;
+nroTestes: Número de testes que serão realizados com a rede;
+porcValidacao: Porcentagens de dados de treinamento que serão utilizados
+para validação;
+Saídas salvas em arquivos
+EQMmedioSerie.mat: Média dos EQM's testes realizados para uma dada série
+EQMdesvioSerie.mat: Desvio padrão dos EQM's dos testes realizados
+nepConvergenciaMedia: Média de épocas necessárias para a rede convergir;
+nepConvergenciaDesvio: Desvio padrão do número de épocas para convergir;
+%}
+function testaMlp(serie,h,lag,nepMax,alfaInicial,nroTestes,porcValidacao)
+	datasetTreinamento = load("Dataset_series/serie" + serie + "_trein.txt");
+	datasetTeste = load("Dataset_series/serie"+ serie + "_test.txt");
+	[Xtr,Ydtr,Xvl,Ydvl,Xts] = processaDados(datasetTreinamento, datasetTeste, lag, porcValidacao);
+	Ydts = datasetTeste(2:end,1);
+	EQMtemp = zeros(1,nroTestes);
+	nepConvergenciaTemp = zeros(1,nroTestes);
+	parfor i=1:nroTestes
+		fprintf("serie: %d h: %d lag: %d teste: %d\n",serie, h,lag,i);
+		mlp = Mlp(h,nepMax,alfaInicial);
+		mlp.treinamento(Xtr,Ydtr,Xvl,Ydvl);
+		mlp.teste(Xts);
+		erro = mlp.Y(1:end-1,1) - Ydts;
+		EQMtemp(i) = 1/size(Xts,1)*sum(sum(erro.*erro));
+		nepConvergenciaTemp(i) = mlp.nepConvergencia;
+	end
+	EQMmedio = mean(EQMtemp);
+	EQMdesvio = std(EQMtemp);
+	save("EQMmedio" + serie + ".mat","EQMmedio");
+	save("EQMdesvio" + serie + ".mat","EQMdesvio");
+	nepConvergenciaMedia = mean(nepConvergenciaTemp);
+	nepConvergenciaDesvio = std(nepConvergenciaTemp);
+	save("nepConvergenciaMedia" + serie + ".mat","nepConvergenciaMedia");
+	save("nepConvergenciaDesvio" + serie + ".mat","nepConvergenciaDesvio");
 end
